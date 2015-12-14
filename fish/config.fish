d51ca341450eff200a2fc3dd9c7ed5673d0f65db
Tim Nieradzik <tim@metastack.pl>
2015-12-14 21:32:03 +0100

    Improve Fish configuration

--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,39 @@
-fish_vi_mode
-alias ls='ls --color=auto'
-
+# Git
 set __fish_git_prompt_show_informative_status 'yes'
 set __fish_git_prompt_color_branch yellow
 function fish_prompt
- set last_status $status
+  set last_status $status
 
- set_color $fish_color_cwd
- printf '%s' (prompt_pwd)
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
 
- set_color normal
- printf '%s ' (__fish_git_prompt)
+  set_color normal
+  printf '%s ' (__fish_git_prompt)
 
- set_color normal
+  set_color normal
 end
 
+# Lua/Torch
+set PATH $PATH $HOME/.luarocks/bin
+set PATH $PATH $HOME/torch/install/bin
+set LD_LIBRARY_PATH $LD_LIBRARY_PATH $HOME/torch/install/lib
+set DYLD_LIBRARY_PATH $DYLD_LIBRARY_PATH $HOME/torch/install/lib
+
+# Java/Scala
+set JAVA_OPTS $JAVA_OPTS "
+  -Dscala.color
+  -XX:InitialHeapSize=1024m
+  -XX:MaxHeapSize=4096m
+  -XX:+CMSClassUnloadingEnabled
+"
+set SBT_OPTS $JAVA_OPTS
+
+# Vim
+fish_vi_mode
+set EDITOR nvim
+
+# Aliases
 alias e vi
 alias g git
 alias p mpv
+alias ls "ls --color=auto"
