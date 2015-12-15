75531e0fa6c41acf6574e1a08fe18315cf715709
Tim Nieradzik <tim@metastack.pl>
2015-12-15 16:54:25 +0100

    Add inputrc

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/mpv ~/.mpv
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/konsole ~/.local/share/konsole
+ln -s ~/dotfiles/inputrc ~/.inputrc
