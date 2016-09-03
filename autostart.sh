1dd07a867ad362c02d44b70832a9d330918766ab
Tim Nieradzik <tim@metastack.pl>
2016-09-03 16:38:46 +0300

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ~/dotfiles/keyboard.sh &
-/usr/lib/gnome-settings-daemon/gnome-settings-daemon &
-gnome-screensaver &
-nm-applet &
-kmix &
+nitrogen --restore &
+trojita &
+pcmanfm-qt -d &
+lxqt-connman-applet &
