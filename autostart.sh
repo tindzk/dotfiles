9528a291d212a630fb707d3169c8eba9ef6f2d25
Tim Nieradzik <tim@sparse.tech>
2017-11-08 19:23:34 +0100

    autostart: Use NetworkManager and configure DPI

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 $HOME/dotfiles/keyboard.sh
+xrandr --dpi 192 &
+
 $HOME/.config/polybar/launch.sh
-nitrogen --set-auto --random $HOME/Wallpapers/
+nitrogen --set-scaled --random $HOME/Wallpapers/ &
 albert &
 redshift &
+
 # lxqt-connman-applet &
+/usr/lib/notification-daemon-1.0/notification-daemon &
+nm-applet --sm-disable &
