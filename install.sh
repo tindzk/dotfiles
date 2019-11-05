ed89b143eeaf7363815a0c6497ce763a9c875204
Tim Nieradzik <tim@sparse.tech>
2019-11-05 22:38:19 +0000

    Add WiFi reconnect script

--- install.sh
+++ install.sh
@@ -46,6 +46,7 @@ ln -s $(which xdg-open) ~/bin/open
 ln -s ~/dotfiles/bin/chromium ~/bin/chromium
 ln -s ~/dotfiles/bin/lock ~/bin/lock
 ln -s ~/dotfiles/bin/matlab ~/bin/matlab
+ln -s ~/dotfiles/bin/reconnect ~/bin/reconnect
 
 mkdir -p ~/.config/git/
 ln -s ~/dotfiles/gitattributes ~/.config/git/attributes
