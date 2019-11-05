e781a97caabd72a2bd97518235ec0c5ab31e5a90
Tim Nieradzik <tim@sparse.tech>
2019-11-05 22:47:41 +0000

    Add Firefox development environment

--- install.sh
+++ install.sh
@@ -45,6 +45,7 @@ mkdir ~/bin
 ln -s $(which xdg-open) ~/bin/open
 ln -s ~/dotfiles/bin/chromium ~/bin/chromium
 ln -s ~/dotfiles/bin/chromium-dev ~/bin/chromium-dev
+ln -s ~/dotfiles/bin/firefox-dev ~/bin/firefox-dev
 ln -s ~/dotfiles/bin/lock ~/bin/lock
 ln -s ~/dotfiles/bin/matlab ~/bin/matlab
 ln -s ~/dotfiles/bin/reconnect ~/bin/reconnect
