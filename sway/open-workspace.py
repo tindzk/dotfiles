a81bec65a1ab8627c83f6b32d24b37c556309fee
Tim Nieradzik <tim@sparse.tech>
2020-12-31 13:16:49 +0100

    sway: Bind workspaces 1-5 to external monitor

--- sway/open-workspace.py
+++ sway/open-workspace.py
@@ -8,7 +8,12 @@ import sys, json, subprocess
 output = subprocess.check_output(['swaymsg', '-t', 'get_workspaces'])
 workspaces = json.loads(output)
 
-next_num = next(i for i in range(1, 100) if not [ws for ws in workspaces if ws['num'] == i])
+focused = [ws for ws in workspaces if ws['focused']][0]
+focused = int(focused['name'][0])
+
+# See `config` for workspace allocation
+lo, hi = (1, 5) if focused >= 1 and focused <= 5 else (6, 10)
+next_num = next(i for i in range(lo, hi + 1) if not [ws for ws in workspaces if ws['num'] == i])
 
 if 'move' in sys.argv:
     subprocess.call(['swaymsg', 'move container to workspace number %i' % next_num])
