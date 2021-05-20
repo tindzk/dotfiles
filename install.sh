fe5b8bcd8864a70a1a09dd7218c4b7afecfb3a88
Tim Nieradzik <tim@sparse.tech>
2021-05-20 18:36:35 +0300

    xkb:

--- install.sh
+++ install.sh
@@ -15,9 +15,11 @@ mkdir -p ~/.xkb/symbols/
 # Sway
 ln -s ~/dotfiles/xkb/symbols/us-custom ~/.xkb/symbols/us-custom
 ln -s ~/dotfiles/xkb/symbols/ua-custom ~/.xkb/symbols/ua-custom
+ln -s ~/dotfiles/xkb/symbols/tr-custom ~/.xkb/symbols/tr-custom
 # i3
-ln -s ~/dotfiles/xkb/symbols/us-custom /usr/share/X11/xkb/symbols/us-custom
-ln -s ~/dotfiles/xkb/symbols/ua-custom /usr/share/X11/xkb/symbols/ua-custom
+sudo ln -s ~/dotfiles/xkb/symbols/us-custom /usr/share/X11/xkb/symbols/us-custom
+sudo ln -s ~/dotfiles/xkb/symbols/ua-custom /usr/share/X11/xkb/symbols/ua-custom
+sudo ln -s ~/dotfiles/xkb/symbols/tr-custom /usr/share/X11/xkb/symbols/tr-custom
 
 mkdir -p ~/.config/zathura/
 ln -s ~/dotfiles/zathurarc ~/.config/zathura/zathurarc
